247a7496941a7954008fe78f86a00b5b019d6cca
Alexey <147872929+arfesik@users.noreply.github.com>
2023-11-09 16:53:19 +0300

    Add files via upload

--- Phone_book/phonebook.h
+++ Phone_book/phonebook.h
@@ -4,6 +4,7 @@
 #include <stdlib.h>
 #include <string.h>
 
+// Структура для хранения данных
 typedef struct PhoneBookData_ {
     char name[32];
     char surname[32];
@@ -25,17 +26,21 @@ typedef struct PhoneBookListDescriptor_ {
 
 PhoneBookListDescriptor* InitPhoneBookDescriptor();
 PhoneBookData* PhoneCard(char* name, char* surname, char* phone);
+PhoneBookListDescriptor* GeneralSearch(PhoneBookListDescriptor* list, char * searchbuff, int choice);
 
 char* GetListRecord(PhoneBookNode *node);
 
 void ShowMenu(PhoneBookListDescriptor* descriptor_);
-void CallMenu1(PhoneBookListDescriptor* descriptor_);
+void CallSubMenu1(PhoneBookListDescriptor* descriptor_);
+void CallSubMenu2(PhoneBookListDescriptor* descriptor_);
 void Clear();
 void DeleteList(PhoneBookListDescriptor* list);
 void AddPhoneBookNode(PhoneBookListDescriptor* descriptor_, PhoneBookData* data_);
-void PrintTop();
-void PrintPhoneBook(PhoneBookListDescriptor* list);
+void PrintTop(int type_);
+void PrintPhoneBook(PhoneBookListDescriptor* list, int choice);
 void WriteAllToFile(PhoneBookListDescriptor* list);
+void CreateListFromFile(PhoneBookListDescriptor *list);
+
 
 
 #endif // PHONEBOOK_H
